cce464234bee008bccc454b1a1ecbfb2c8150d42
C0A22121 <c0a22121a0@edu.teu.ac.jp>
2023-11-21 17:16:51 +0900

    課題

--- pygame_image.py
+++ pygame_image.py
@@ -8,24 +8,30 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     bg_img2 = pg.transform.flip(bg_img, True, False)
-    tmr = 0
+    tmr = 0 
 
     kou1 = pg.image.load("ex01/fig/3.png")
     kou1 = pg.transform.flip(kou1, True, False)
-    kou2 = pg.transform.rotozoom(kou1, 10, 1.0)
-    kou_list = [kou1, kou2]
-
+    kou_list = [kou1]
+    for i in range(20):
+        kou_list.append(pg.transform.rotozoom(kou1, i/2, 1.0))
+    for i in range(20):
+        kou_list.append(pg.transform.rotozoom(kou1, 10-(i/2), 1.0))
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr%1600
+        x = tmr%3200
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img2 , [1600-x, 0])
-        screen.blit(kou_list[tmr%2], [300, 200])
+        screen.blit(bg_img2, [1600-x, 0])
+        screen.blit(bg_img, [3200-x, 0])
+        screen.blit(kou_list[tmr%40], [300, 200])
         pg.display.update()
-        tmr += 1        
-        clock.tick(100)
+        tmr += 1
+        if tmr == 3199:
+            tmr = 0
+        clock.tick(1000)
 
 
 if __name__ == "__main__":
